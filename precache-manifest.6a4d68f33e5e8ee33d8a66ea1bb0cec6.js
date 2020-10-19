481fcfcd4af6aa8aeb57b1166c57a9bea7ad3a78
Egberto Berlingeri <berlingeri.egberto@protonmail.com>
2020-10-19 06:43:41 -0400

    Updates

---
+++ precache-manifest.6a4d68f33e5e8ee33d8a66ea1bb0cec6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef751fc142e08f8752849940d1b1c49",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "adb87e27dc62ee6cd3dc",
+    "url": "/monsters-rolodex/static/css/main.b464d8ce.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb87e27dc62ee6cd3dc",
+    "url": "/monsters-rolodex/static/js/main.fd9c9b7b.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
